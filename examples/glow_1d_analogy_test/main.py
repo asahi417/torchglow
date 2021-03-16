f4eab98f6021192ed257d4d8d7635eb15a896e28
asahi417 <asahi1992ushio@gmail.com>
2021-03-16 00:30:15 +0000

    fix

--- examples/glow_1d_analogy_test/main.py
+++ examples/glow_1d_analogy_test/main.py
@@ -46,51 +46,57 @@ def get_options():
     parser = argparse.ArgumentParser(description='Train Glow model on built-in dataset.')
     # model parameter
     parser.add_argument('--checkpoint-path', help='model checkpoint', required=True, type=str)
-    parser.add_argument('-o', '--output-dir', help='directory to export model weight file', default='./ckpt', type=str)
+    parser.add_argument('-o', '--output-dir', help='directory to export model weight file',
+                        default='./examples/glow_1d_analogy_test', type=str)
     parser.add_argument('-b', '--batch', help='batch size', default=128, type=int)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     opt = get_options()
-    epochs = [i.split('model.')[-1].replace('.pt', '') for i in glob('{}/model.*.pt'.format(opt.checkpoint_path))]
+    checkpoint_paths = opt.checkpoint_path.split(',')
     result = []
-    for e in epochs + [None]:
-        if e is not None:
-            model = torchglow.GlowWordEmbedding(checkpoint_path=opt.checkpoint_path, checkpoint_option={'epoch': e})
-        else:
-            model = torchglow.GlowWordEmbedding(checkpoint_path=opt.checkpoint_path)
+    for checkpoint_path in checkpoint_paths:
+        epochs = [i.split('model.')[-1].replace('.pt', '') for i in glob('{}/model.*.pt'.format(checkpoint_path))]
+        for e in epochs + [None]:
+            if e is not None:
+                model = torchglow.GlowWordEmbedding(checkpoint_path=checkpoint_path, checkpoint_option={'epoch': e})
+            else:
+                model = torchglow.GlowWordEmbedding(checkpoint_path=checkpoint_path)
 
-        for i in DATA:
-            tmp_result = {'model_type': model.config.model_type, 'data': i, 'epoch': e, 'ckpt': opt.checkpoint_path}
-            val, test = get_dataset_raw(i)
-            all_pairs = list(chain(*[[o['stem']] + o['choice'] for o in val + test]))
-            all_pairs_format = ['__'.join(p).replace(' ', '_').lower() for p in all_pairs]
-            all_pairs_format = list(filter(lambda x: x in model.vocab(), all_pairs_format))
-            vector = model.embed(all_pairs_format, batch=opt.batch)
-            latent_dict = {k: v for k, v in zip(all_pairs_format, vector)}
+            for i in DATA:
+                tmp_result = {'ckpt': checkpoint_path, 'data': i, 'epoch': e}
+                val, test = get_dataset_raw(i)
+                all_pairs = list(chain(*[[o['stem']] + o['choice'] for o in val + test]))
+                all_pairs_format = ['__'.join(p).replace(' ', '_').lower() for p in all_pairs]
+                all_pairs_format = list(filter(lambda x: x in model.vocab(), all_pairs_format))
+                vector = model.embed(all_pairs_format, batch=opt.batch)
+                latent_dict = {k: v for k, v in zip(all_pairs_format, vector)}
 
-            def get_prediction(single_data):
-                stem = '__'.join(single_data['stem']).replace(' ', '_').lower()
-                choice = ['__'.join(c).replace(' ', '_').lower() for c in single_data['choice']]
-                if stem not in model.vocab():
-                    return None
-                sim = [cos_similarity(latent_dict[stem], latent_dict[c]) if c in latent_dict else -100 for c in choice]
-                pred = sim.index(max(sim))
-                if sim[pred] == -100:
-                    return None
-                return pred
+                def get_prediction(single_data):
+                    stem = '__'.join(single_data['stem']).replace(' ', '_').lower()
+                    choice = ['__'.join(c).replace(' ', '_').lower() for c in single_data['choice']]
+                    if stem not in model.vocab():
+                        return None
+                    sim = [cos_similarity(latent_dict[stem], latent_dict[c]) if c in latent_dict else -100 for c in choice]
+                    pred = sim.index(max(sim))
+                    if sim[pred] == -100:
+                        return None
+                    return pred
 
 
-            for prefix, data in zip(['test', 'valid'], [test, val]):
-                prediction = [get_prediction(o) for o in data]
-                tmp_result['oov_{}'.format(prefix)] = len([p for p in prediction if p is None])
-                prediction = [p if p is not None else BASE_PREDICTION[data][prefix][n] for n, p in enumerate(prediction)]
-                accuracy = [o['answer'] == p for o, p in zip(data, prediction)]
-                tmp_result['accuracy_{}'.format(prefix)] = sum(accuracy)/len(accuracy)
-            tmp_result['accuracy'] = (tmp_result['accuracy_test'] * len(test) +
-                                      tmp_result['accuracy_valid'] * len(val)) / (len(val) + len(test))
-            result.append(tmp_result)
-    print(pd.DataFrame(result))
+                for prefix, data in zip(['test', 'valid'], [test, val]):
+                    prediction = [get_prediction(o) for o in data]
+                    tmp_result['oov_{}'.format(prefix)] = len([p for p in prediction if p is None])
+                    prediction = [p if p is not None else BASE_PREDICTION[data][prefix][n] for n, p in enumerate(prediction)]
+                    accuracy = [o['answer'] == p for o, p in zip(data, prediction)]
+                    tmp_result['accuracy_{}'.format(prefix)] = sum(accuracy)/len(accuracy)
+                tmp_result['accuracy'] = (tmp_result['accuracy_test'] * len(test) +
+                                          tmp_result['accuracy_valid'] * len(val)) / (len(val) + len(test))
+                result.append(tmp_result)
+    df = pd.DataFrame(result)
+    df = df.sort_value(by=['data', 'ckpt', 'epoch'])
+    print(df)
+    df.to_csv('{}/result.csv'.format(opt.output_dir))
     # pd.DataFrame(tmp_result).tocsv('./result.csv')
     # logging.info('finish evaluation: result was exported to {}'.format('./result.jsonl'))
