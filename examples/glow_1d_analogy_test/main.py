f408162e9a4e660e4ce2c2c9d39d867eef239689
asahi417 <asahi1992ushio@gmail.com>
2021-03-16 00:17:48 +0000

    fix

--- examples/glow_1d_analogy_test/main.py
+++ examples/glow_1d_analogy_test/main.py
@@ -65,7 +65,6 @@ if __name__ == '__main__':
             tmp_result = {'model_type': model.config.model_type, 'data': i, 'epoch': e, 'ckpt': opt.checkpoint_path}
             val, test = get_dataset_raw(i)
             all_pairs = list(chain(*[[o['stem']] + o['choice'] for o in val + test]))
-            print(all_pairs)
             all_pairs_format = ['__'.join(p).replace(' ', '_').lower() for p in all_pairs]
             all_pairs_format = list(filter(lambda x: x in model.vocab(), all_pairs_format))
             vector = model.embed(all_pairs_format, batch=opt.batch)
