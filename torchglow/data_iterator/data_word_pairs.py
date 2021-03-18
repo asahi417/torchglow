6d9f9a96efd9d7212d39c5200b15c034f33477d1
asahi417 <asahi1992ushio@gmail.com>
2021-03-18 18:26:27 +0000

    fix

--- torchglow/data_iterator/data_word_pairs.py
+++ torchglow/data_iterator/data_word_pairs.py
@@ -54,6 +54,9 @@ def get_dataset_word_pairs(data_iterator,
         # convert word pair to word-level data_iterator
         data = list(set(list(chain(*data))))
 
+    if data_iterator.model_vocab is not None:
+        data = list(filter(lambda x: x in data_iterator.model_vocab, data))
+
     if validation_rate == 0:
         return data_iterator(data, parallel=parallel), None
     n = int(len(data) * validation_rate)
