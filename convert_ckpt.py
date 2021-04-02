5d563eca9e36edda00af80978d26ac577146f0c9
asahi417 <asahi1992ushio@gmail.com>
2021-04-02 12:23:15 +0100

    fix

--- convert_ckpt.py
+++ convert_ckpt.py
@@ -1,7 +1,7 @@
 import torchglow
 from glob import glob
 
-ckpts = glob('./ckpt/image/cifar/model*')
+ckpts = glob('./ckpt/image/cifar10/model*')
 for i in ckpts:
     epoch = int(i.split('model.')[-1].replace('.pt', ''))
     print('loading from {}'.format(epoch))
