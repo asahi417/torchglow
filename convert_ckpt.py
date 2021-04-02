e9ea44044d247c2768aeb937bc0326264f43f32e
asahi417 <asahi1992ushio@gmail.com>
2021-04-02 13:46:03 +0100

    fix

--- convert_ckpt.py
+++ convert_ckpt.py
@@ -4,10 +4,9 @@ from glob import glob
 ckpts = glob('./ckpt/image/cifar10/model*')
 for i in ckpts:
     epoch = int(i.split('model.')[-1].replace('.pt', ''))
-    if epoch < 1300:
-        continue
-    print('loading from {}'.format(epoch))
-    try:
-        torchglow.Glow(checkpoint_path='./ckpt/image/cifar10', checkpoint_epoch=epoch)
-    except ValueError:
-        print('DONE')
+    if epoch > 1300:
+        print('loading from {}'.format(epoch))
+        try:
+            torchglow.Glow(checkpoint_path='./ckpt/image/cifar10', checkpoint_epoch=epoch)
+        except ValueError:
+            print('DONE')
