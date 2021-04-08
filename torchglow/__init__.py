c7bede06144b55e5b725b3aa0077ba9a27e0d365
asahi417 <asahi1992ushio@gmail.com>
2021-04-08 23:23:59 +0100

    fix

--- torchglow/__init__.py
+++ torchglow/__init__.py
@@ -1,6 +1,3 @@
 from .glow.model_image import Glow
-from .glow.model_word import GlowWordEmbedding
-from .glow.model_bert import GlowBERT
-from .data_iterator import language_models as lm
 from .data_iterator import get_image_decoder, get_dataset_image
 from . import util, data_iterator
--- torchglow/data_iterator/__init__.py
+++ torchglow/data_iterator/__init__.py
@@ -1,2 +1,1 @@
 from .data_image import get_dataset_image, get_image_decoder
-from .data_word_pairs import get_dataset, get_iterator_bert, get_iterator_word_embedding
